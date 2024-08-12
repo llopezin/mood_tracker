6bd522aca5958f7d0cd9a03dcf4f93dd922f5e89
Laura <laura.lopez@hyd.agency>
2024-08-12 12:18:09 +0200

    fix: fetches mood list using suspense

--- app/src/app/registry/page.tsx
+++ app/src/app/registry/page.tsx
@@ -1,16 +1,13 @@
-import { GetMoodsQueryDocument } from "@/generated/graphql";
-import { query } from "@/service/apollo";
-import React from "react";
+import MoodList from "@/components/moodList";
+import React, { Suspense } from "react";
 
-export default async function Page() {
-  const { data } = await query({
-    query: GetMoodsQueryDocument,
-  });
-
+export default function Page() {
   return (
     <>
       <h1>Past moods</h1>
-      {JSON.stringify(data)}
+      <Suspense fallback="Loading...">
+        <MoodList />
+      </Suspense>
     </>
   );
 }
