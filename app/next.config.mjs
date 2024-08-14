4fbe5b04bc1d4a8776bb624bc69b4311df435f51
Laura <laura.lopez@hyd.agency>
2024-08-14 09:59:26 +0200

    feat: redirects depending on user status

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,46 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import cookieNames from "./cookieNames.mjs"
+
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        missing: [
+          {
+            type: "cookie",
+            key: cookieNames.token,
+          },
+        ],
+        permanent: false,
+        destination: "/login",
+      },
+      {
+        source: "/login",
+        missing: [
+          {
+            type: "cookie",
+            key: cookieNames.token,
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+      {
+        source: "/sign-up",
+        missing: [
+          {
+            type: "cookie",
+            key: cookieNames.token,
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
